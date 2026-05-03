5d80ee98b36628ae08503567494dc640963f4cc1
devworks75 <32442670+devworks75@users.noreply.github.com>
2026-05-03 10:17:26 -0500

    file 7 added

---
+++ first7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script displays the current directory and lists all files
+#Added by Phani Tenneti
+# Dt 26th April
+#This is file7
+echo "Current Directory:"
+pwd
+
+echo "Files in Directory:"
+ls -l
