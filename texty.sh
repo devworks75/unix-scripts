bffe57508c760477ca1418cadf5c5f46fd761526
devworks75 <microsoftdevloper75@gmail.com>
2026-07-19 12:01:20 -0500

    seconf file y

--- texty.sh
+++ texty.sh
@@ -1,9 +1,4 @@
 #this is s a unix sample file
  this is in file y
-<<<<<<< HEAD
-   this is in dev branch
-line for merege conflict
-=======
  this is in main and dev branches
  adding a line for merege conflict
->>>>>>> main
