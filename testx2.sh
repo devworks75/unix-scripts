9679b61b8eb01f86b8f1d8ebc61aec0f35687612
devworks75 <microsoftdevloper75@gmail.com>
2026-07-19 18:43:01 -0500

    new file

---
+++ testx2.sh
@@ -0,0 +1,19 @@
+#this is s a unix sample file
+added a line for dev branch
+added a line for merge conflict
+created a new branch
+createa new brnach for MR
+test MR reuqest
+this is on brnach dev
+adding a new line for merge conflict
+test MR reuqest
+added a line for dev and main branch
+added a line for merge conflicts
+created a new branch
+this is on brnach dev
+adding a new line for merge conflict
+createa new brnach for MR
+test MR reuqest
+added a line for dev and main branch
+added a line for merge conflicts
+
