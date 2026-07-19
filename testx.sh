390dba45ceaf630e007af842ce80ecc665840c81
devworks75 <microsoftdevloper75@gmail.com>
2026-07-19 18:17:24 -0500

    added a new line for merge conflict

--- testx.sh
+++ testx.sh
@@ -3,6 +3,7 @@ added a line for dev branch
 added a line for merge conflict
 created a new branch
 this is on brnach dev
+adding a new line for merge conflict
 test MR reuqest
 added a line for dev and main branch
 added a line for merge conflicts
