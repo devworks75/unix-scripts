967f6f963a94bb885b837d8599646d28a6bf4b35
devworks75 <microsoftdevloper75@gmail.com>
2026-07-19 18:58:33 -0500

    added testy.sh

---
+++ texty.sh
@@ -0,0 +1,9 @@
+#this is s a unix sample file
+added a line for dev branch
+added a line for merge conflict
+this is on brnach dev
+adding a new line for merge conflict
+test MR reuqest
+added a line for dev and main branch
+added a line for merge conflicts
+
